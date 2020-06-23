6193cef7578ae8b60836f795ec2132a1ed969e17
domuk-k <dannyworks102@gmail.com>
2020-06-23 18:56:07 +0900

    database: model & custome routers

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-
-// server.js
-// const cors = require('cors');
 const jsonServer = require('json-server');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({});
-
-
+const middlewares = jsonServer.defaults();
 // db.json를 조작하기 위해 lowdb를 사용
+// https://github.com/typicode/lowdb
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('db.json');
@@ -15,21 +14,71 @@ const db = low(adapter);
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
-// server.use(cors());
-
-server.get('user/board', (req, res) => {
-  console.log(req)
-  console.log(res)
-})
-
-// .remove({ list_id: req.params.list_id })
-// .write();
+server.use(jsonServer.bodyParser)
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(cors());
 // Add custom routes before JSON Server router
 
-// lowdb를 사용해서 db.json에서 completed: true인 todo를 제거
+// GET
+// get boards
+server.get('/boards', (req, res) => {
+  res.send(
+    db.get(`users[0].boards`)
+      .value()
+  )
+})
+// get a board
+server.get('/boards/:board_id', (req, res) => {
+  res.send(
+    db.get(`users[0].boards[${req.params.board_id - 1}]`)
+      .value()
+  )
+})
+// get lists
+server.get('/boards/:board_id/lists', (req, res) => {
+  res.send(
+    db.get(`users[0].boards[${req.params.board_id - 1}].lists`)
+      .value()
+  )
+})
+// get a list
+server.get('/boards/:board_id/lists/:list_id', (req, res) => {
+  res.send(
+    db.get(`users[0].boards[${req.params.board_id - 1}].lists[${req.params.list_id - 1}]`)
+      .value()
+  )
+})
 
-// todos를 응답
+//POST
+// push a board
+server.post('/boards/', (req, res) => {
+  db.get(`users[0].boards`)
+    .push(req.body)
+    .write()
+  res.send(
+    // get boards
+    db.get(`users[0].boards`).value()
+  )
+})
+// push a list
+server.post('/boards/:board_id/lists/', (req, res) => {
+  db.get(`users[0].boards[${req.params.board_id - 1}].lists`)
+    .push(req.body)
+    .write()
+  res.send(
+    req.body
+  )
+})
 
+
+// server.delete('/todos/completed', (req, res) => {
+//   // lowdb를 사용해서 db.json에서 completed: true인 todo를 제거
+//   db.get('todos')
+//     .remove({ completed: true })
+//     .write();
+//   // todos를 응답
+//   res.send(db.get('todos').value());
+// })
 
 // Use default router
 server.use(router);
